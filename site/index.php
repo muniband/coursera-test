ab7765a7c47d761efcb2fc6fedc6532b34c48519
Muni Bandlamoori <muni@us.ibm.com>
2021-06-06 18:14:26 -0700

    my index.php

---
+++ site/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>php3</title>
+</head>
+<body>
+
+	<h1> test1 php </h1>
+<form method="GET">
+	<input type="text" name="person">
+	<button>SUBMIT</button>
+</form>
+
+<?php 
+  //$name = $_GET['person'] ; 
+  /*echo $name . " is my nickname ";  
+   
+  echo strlen("Hi world");
+  echo  str_word_count("this is my first php");
+  echo  strrev("this is my first php");
+  echo  strpos("this is my first php","php");
+  echo  str_replace("php","html","this is my first php");
+
+  */
+  
+
+?>
+</body>
+</html>
